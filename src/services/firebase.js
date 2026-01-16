fd17e80e63142ce151da8f7d2e92ff61e945b3ac
prithvijedipatil <prithvipatil09@gmail.com>
2026-01-16 18:13:31 +0530

    deploy

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,12 +9,12 @@ import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCYVJ8pWsFCNku7MxDoKmdNh1phqqMyLJI",
-  authDomain: "the-rahil-s-d3cdd.firebaseapp.com",
-  projectId: "the-rahil-s-d3cdd",
-  storageBucket: "the-rahil-s-d3cdd.firebasestorage.app",
-  messagingSenderId: "55219823426",
-  appId: "1:55219823426:web:650f0e1cbe66af69859edb",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
